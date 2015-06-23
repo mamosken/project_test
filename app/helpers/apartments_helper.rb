52a9c0f5a9af152fc4cc62d499888b10f9d8b0c9
mamosken <marcelamosken@gmail.com>
2015-06-22 20:07:57 -0400

    add user validations, implemented sorting by neighborhood links, created Category model and populated options

--- app/helpers/apartments_helper.rb
+++ app/helpers/apartments_helper.rb
@@ -1,2 +1,3 @@
 module ApartmentsHelper
+
 end
--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,5 @@
 class Apartment < ActiveRecord::Base
+
 	belongs_to :user
 	has_many :rooms, dependent: :destroy
 	has_many :preferences
@@ -19,5 +20,7 @@ class Apartment < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "80x80>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	belongs_to :category
 end
 
